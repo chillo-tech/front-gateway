e318361fafe205ae94886e2e9c2981bf7e44795d
Achille <achille.mbougueng@chillo.tech>
2024-02-10 04:47:21 +0100

    deploiement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import Button from './Button'
 import { useContext } from 'react'
 import { ApplicationContext } from '../context/ApplicationContextProvider'
+import useHeader from './useHeader'
 
 function Header() {
   const {isLoggedIn, updateLogin} = useContext(ApplicationContext)
+  useHeader();
   return (
     <header style={{display: 'flex', justifyContent: 'space-between'}}>
         <a href='/'>LOGO</a>
